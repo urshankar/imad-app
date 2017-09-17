a3c5dccd4c1613f904eeca815c2c501be5f507b4
urshankar <urshankar@gmail.com>
2017-09-17 20:03:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article-one', function (req, res) {
+  res.send('Send article one');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Send article two');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Send article three');
+});
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
